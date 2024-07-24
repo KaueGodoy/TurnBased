d08b8c8e02133b9176d78ee2df4d7206b18d3088
Kaue Godoy <105384509+KaueGodoy@users.noreply.github.com>
2024-07-24 13:01:50 -0300

    UnitSelected

--- TurnBased/Assets/_Scripts/UnitActionSystem.cs
+++ TurnBased/Assets/_Scripts/UnitActionSystem.cs
@@ -3,13 +3,31 @@ using UnityEngine;
 public class UnitActionSystem : MonoBehaviour
 {
     [SerializeField] private Unit _selectedUnit;
+    [SerializeField] private LayerMask _unitLayerMask;
 
     private void Update()
     {
-
         if (Input.GetMouseButtonDown(0))
         {
             _selectedUnit.Move(MouseWorld.GetPosition());
         }
+
+        if (Input.GetMouseButton(1))
+        {
+            HandleUnitSelection();
+        }
+    }
+
+    private void HandleUnitSelection()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, _unitLayerMask))
+        {
+            if (raycastHit.transform.TryGetComponent<Unit>(out Unit unit))
+            {
+                _selectedUnit = unit;
+            }
+        }
+
     }
 }
