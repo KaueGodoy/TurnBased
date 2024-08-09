d6542604cf7c64a4b38d25157db9e253da2b2eca
Kaue Godoy <105384509+KaueGodoy@users.noreply.github.com>
2024-08-09 09:54:21 -0300

    CameraZoom

--- TurnBased/Assets/CameraController.cs
+++ TurnBased/Assets/CameraController.cs
@@ -1,11 +1,31 @@
+using Cinemachine;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CameraController : MonoBehaviour
 {
+    private const float Min_Follow_Y_Offset = 2f;
+    private const float Max_Follow_Y_Offset = 12f;
+
+    [SerializeField] private CinemachineVirtualCamera _cinemachineVirtualCamera;
+
     [SerializeField] private float _moveSpeed = 10f;
     [SerializeField] private float _rotationSpeed = 50f;
+    [SerializeField] private float _zoomAmount = 1f;
+    [SerializeField] private float _zoomSpeed = 3f;
     public float MoveSpeed { get { return _moveSpeed; } set { value = _moveSpeed; } }
     public float RotationSpeed { get { return _rotationSpeed; } set { value = _rotationSpeed; } }
+    public float ZoomAmount { get { return _zoomAmount; } set { value = _zoomAmount; } }
+    public float ZoomSpeed { get { return _zoomSpeed; } set { value = _zoomSpeed; } }
+
+    private CinemachineTransposer _cinemachineTransposer;
+    private Vector3 _targetFollowOffset;
+
+    private void Start()
+    {
+        _cinemachineTransposer = _cinemachineVirtualCamera.GetCinemachineComponent<CinemachineTransposer>();
+        _targetFollowOffset = _cinemachineTransposer.m_FollowOffset;
+    }
 
     private void Update()
     {
@@ -43,5 +63,21 @@ public class CameraController : MonoBehaviour
         }
 
         transform.eulerAngles += rotationVector * RotationSpeed * Time.deltaTime;
+
+        if (Input.mouseScrollDelta.y > 0)
+        {
+            _targetFollowOffset.y -= ZoomAmount;
+        }
+
+        if (Input.mouseScrollDelta.y < 0)
+        {
+            _targetFollowOffset.y += ZoomAmount;
+        }
+
+        _targetFollowOffset.y = Mathf.Clamp(_targetFollowOffset.y, Min_Follow_Y_Offset, Max_Follow_Y_Offset);
+        _cinemachineTransposer.m_FollowOffset = Vector3.Lerp(_cinemachineTransposer.m_FollowOffset, _targetFollowOffset, Time.deltaTime * ZoomSpeed);
+
+        Debug.Log($"Cinemachine {_cinemachineTransposer.m_FollowOffset}, + Target {_targetFollowOffset}");
+
     }
 }
