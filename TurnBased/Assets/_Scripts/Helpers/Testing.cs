b511853c03f8ae2eefaacff7901fee73da108e77
Kaue Godoy <105384509+KaueGodoy@users.noreply.github.com>
2024-08-14 10:46:38 -0300

    SelectedUnitGridVisual

--- TurnBased/Assets/_Scripts/Helpers/Testing.cs
+++ TurnBased/Assets/_Scripts/Helpers/Testing.cs
@@ -13,7 +13,8 @@ public class Testing : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.T))
         {
-            _unit.GetMoveAction().GetValidActionGridPositionList();
+            GridSystemVisual.Instance.HideAllGridPositions();
+            GridSystemVisual.Instance.ShowGridPositionList(_unit.GetMoveAction().GetValidActionGridPositionList());
         }
     }
 }
