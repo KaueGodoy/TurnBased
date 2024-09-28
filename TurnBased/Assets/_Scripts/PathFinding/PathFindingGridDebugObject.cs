996720e80d70c82efa70851ac3d003656febbcb6
Kaue Godoy <105384509+KaueGodoy@users.noreply.github.com>
2024-09-28 11:37:44 -0300

    LevelDesign

--- TurnBased/Assets/_Scripts/PathFinding/PathFindingGridDebugObject.cs
+++ TurnBased/Assets/_Scripts/PathFinding/PathFindingGridDebugObject.cs
@@ -6,6 +6,7 @@ public class PathFindingGridDebugObject : GridDebugObject
     [SerializeField] private TextMeshPro _gCostText;
     [SerializeField] private TextMeshPro _hCostText;
     [SerializeField] private TextMeshPro _fCostText;
+    [SerializeField] private SpriteRenderer _isWalkableSpriteRenderer;
 
     private PathNode _pathNode;
 
@@ -21,5 +22,6 @@ public class PathFindingGridDebugObject : GridDebugObject
         _gCostText.text = _pathNode.GCost.ToString();
         _hCostText.text = _pathNode.HCost.ToString();
         _fCostText.text = _pathNode.FCost.ToString();
+        _isWalkableSpriteRenderer.color = _pathNode.IsWalkable ? Color.green : Color.red;
     }
 }
