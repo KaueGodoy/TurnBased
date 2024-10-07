455376c6aef7ae400630d334ee3dc2803314554b
Kaue Godoy <105384509+KaueGodoy@users.noreply.github.com>
2024-10-07 12:05:49 -0300

    CrateRagdollPhysics

--- TurnBased/Assets/_Scripts/Props/DestructibleCrate.cs
+++ TurnBased/Assets/_Scripts/Props/DestructibleCrate.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 
 public class DestructibleCrate : MonoBehaviour
 {
+    [SerializeField] private Transform _crateDestroyedPrefab;
+    [SerializeField] private float _explosionForce = 100f;
+    [SerializeField] private float _explosionRange = 10f;
 
     public static event EventHandler OnAnyDestroyed;
 
@@ -16,8 +19,25 @@ public class DestructibleCrate : MonoBehaviour
 
     public void TakeDamage()
     {
+        Transform crateDestroyedTransform = Instantiate(_crateDestroyedPrefab, transform.position, Quaternion.identity);
+
+        ApplyExplosionToChildren(crateDestroyedTransform, _explosionForce, transform.position, _explosionRange);
+
         Destroy(gameObject);
 
         OnAnyDestroyed?.Invoke(this, EventArgs.Empty);
+    }
+
+    private void ApplyExplosionToChildren(Transform root, float explosionForce, Vector3 explosionPosition, float explosionRange)
+    {
+        foreach (Transform child in root)
+        {
+            if (child.TryGetComponent<Rigidbody>(out Rigidbody childRigidBody))
+            {
+                childRigidBody.AddExplosionForce(explosionForce, explosionPosition, explosionRange);
+            }
+
+            ApplyExplosionToChildren(child, explosionForce, explosionPosition, explosionRange);
+        }
     }
 }
