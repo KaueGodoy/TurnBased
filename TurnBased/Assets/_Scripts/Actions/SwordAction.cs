a30a4202f1e6874c2302dcee6bd44e83759ed8b6
Kaue Godoy <105384509+KaueGodoy@users.noreply.github.com>
2024-10-08 11:08:22 -0300

    SwordAction

---
+++ TurnBased/Assets/_Scripts/Actions/SwordAction.cs
@@ -0,0 +1,133 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordAction : BaseAction
+{
+    [SerializeField] private int _maxSwordDistance = 1;
+    [SerializeField] private int _damage = 100;
+    public int MaxSwordDistance { get { return _maxSwordDistance; } set { _maxSwordDistance = value; } }
+    public int Damage { get { return _damage; } set { _damage = value; } }
+
+    private enum State
+    {
+        SwingingSwordBeforeHit,
+        SwingingSwordAfterHit,
+    }
+
+    private State _currentState;
+    private float _stateTimer;
+    private float _afterHitStateTimer = 0.5f;
+    private float _beforeHitStateTimer = 0.7f;
+    private Unit _targetUnit;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        ActionName = "Sword";
+    }
+
+    private void Update()
+    {
+        if (!_isActive) return;
+
+        _stateTimer -= Time.deltaTime;
+
+        switch (_currentState)
+        {
+            case State.SwingingSwordBeforeHit:
+                Vector3 aimingDirection = (_targetUnit.GetWorldPosition() - _unit.GetWorldPosition()).normalized;
+                float rotateSpeed = 10f;
+                transform.forward = Vector3.Lerp(transform.forward, aimingDirection, rotateSpeed * Time.deltaTime);
+                break;
+            case State.SwingingSwordAfterHit:
+                break;
+        }
+
+        if (_stateTimer <= 0f)
+        {
+            NextState();
+        }
+
+    }
+
+    private void NextState()
+    {
+        switch (_currentState)
+        {
+            case State.SwingingSwordBeforeHit:
+                _currentState = State.SwingingSwordAfterHit;
+                _stateTimer = _afterHitStateTimer;
+                _targetUnit.TakeDamage(Damage);
+                break;
+            case State.SwingingSwordAfterHit:
+                ActionComplete();
+                break;
+        }
+    }
+
+    public override string GetActionName()
+    {
+        return ActionName;
+    }
+
+    public override EnemyAIAction GetEnemyAIAction(GridPosition gridPosition)
+    {
+        return new EnemyAIAction
+        {
+            GridPosition = gridPosition,
+            ActionValue = 200,
+        };
+    }
+
+    public override List<GridPosition> GetValidActionGridPositionList()
+    {
+        List<GridPosition> validGridPositionList = new List<GridPosition>();
+
+        GridPosition unitGridPosition = _unit.GetGridPosition();
+
+        for (int x = -MaxSwordDistance; x <= MaxSwordDistance; x++)
+        {
+            for (int z = -MaxSwordDistance; z <= MaxSwordDistance; z++)
+            {
+                GridPosition offsetGridPosition = new GridPosition(x, z);
+                GridPosition testGridPosition = unitGridPosition + offsetGridPosition;
+
+                if (!LevelGrid.Instance.IsValidGridPosition(testGridPosition))
+                {
+                    // grid position with x and z >= 0 and within width and height
+                    continue;
+                }
+
+                if (!LevelGrid.Instance.HasAnyUnitOnGridPosition(testGridPosition))
+                {
+                    // Grid position is empty, no Unit
+                    continue;
+                }
+
+                Unit targetUnit = LevelGrid.Instance.GetUnitAtGridPosition(testGridPosition);
+
+                if (targetUnit.IsEnemy == _unit.IsEnemy)
+                {
+                    // Both Units on the same 'team'
+                    continue;
+                }
+
+                //Debug.Log(testGridPosition);
+                validGridPositionList.Add(testGridPosition);
+            }
+        }
+
+        return validGridPositionList;
+    }
+
+    public override void TakeAction(GridPosition gridPosition, Action onActionComplete)
+    {
+        _targetUnit = LevelGrid.Instance.GetUnitAtGridPosition(gridPosition);
+
+        _currentState = State.SwingingSwordBeforeHit;
+        _stateTimer = _beforeHitStateTimer;
+
+        ActionStart(onActionComplete);
+    }
+}
--- TurnBased/Assets/_Scripts/Grid/GridSystemVisual.cs
+++ TurnBased/Assets/_Scripts/Grid/GridSystemVisual.cs
@@ -81,6 +81,28 @@ public class GridSystemVisual : MonoBehaviour
         }
     }
 
+    private void ShowGridPositionRangeSquare(GridPosition gridPosition, int range, GridVisualType gridVisualType)
+    {
+        List<GridPosition> gridPositionList = new List<GridPosition>();
+
+        for (int x = -range; x < range; x++)
+        {
+            for (int z = -range; z < range; z++)
+            {
+                GridPosition testGridPosition = gridPosition + new GridPosition(x, z);
+
+                if (!LevelGrid.Instance.IsValidGridPosition(testGridPosition))
+                {
+                    continue;
+                }
+
+                gridPositionList.Add(testGridPosition);
+            }
+        }
+
+        ShowGridPositionList(gridPositionList, gridVisualType);
+    }
+
     private void ShowGridPositionRange(GridPosition gridPosition, int range, GridVisualType gridVisualType)
     {
         List<GridPosition> gridPositionList = new List<GridPosition>();
@@ -138,9 +160,14 @@ public class GridSystemVisual : MonoBehaviour
                 break;
             case ShootAction shootAction:
                 gridVisualType = GridVisualType.Red;
-
                 ShowGridPositionRange(selectedUnit.GetGridPosition(), shootAction.MaxShootRange, GridVisualType.RedSoft);
-
+                break;
+            case GrenadeAction grenadeAction:
+                gridVisualType = GridVisualType.Yellow;
+                break;
+            case SwordAction swordAction:
+                gridVisualType = GridVisualType.Red;
+                ShowGridPositionRangeSquare(selectedUnit.GetGridPosition(), swordAction.MaxSwordDistance, GridVisualType.RedSoft);
                 break;
         }
 
